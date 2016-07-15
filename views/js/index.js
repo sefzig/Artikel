e9da2741b8a90e0a20b618fe1737f2ad5c18a16f
Andreas Sefzig <andreas@sefzig.net>
2016-07-15 11:11:54 +0200

    Skript

--- views/js/index.js
+++ views/js/index.js
@@ -261,6 +261,10 @@
                 
              }
              
+             var gut =      getParameters("gut");      if ((gut)      && (gut      != "")) { window.setTimeout(function() {  Smooch.sendMessage(" Finde ich nicht gut: "+schlecht+" "); }, 1000); }
+             var schlecht = getParameters("schlecht"); if ((schlecht) && (schlecht != "")) { window.setTimeout(function() {  Smooch.sendMessage(" Finde ich       gut: "+gut     +" "); }, 1000); }
+             var neutral =  getParameters("neutral");  if ((neutral)  && (neutral  != "")) { window.setTimeout(function() {  Smooch.sendMessage(" Text:                "+neutral +" "); }, 1000); }
+             
           }, 333); }); // Addon nach 22.06.16, vorher 100
           
        // Smooch.open();
